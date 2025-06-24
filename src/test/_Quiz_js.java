75a4bbc78aac64788f3a503b77ede6f4698793ab
Sonsuho0307 <sksh237@naver.com>
2025-06-24 10:19:35 +0900

    6/24

--- src/test/_Quiz_js.java
+++ src/test/_Quiz_js.java
@@ -2,11 +2,26 @@ package test;
 
 public class _Quiz_js {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.println('*');
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print('*');
+            }   System.out.println("");
+
+
+        }
+        System.out.println("--------------");
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 0; j < (5 - i); j++) {
+                System.out.print(" ");
+
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
             }
 
+            System.out.println("");
         }
     }
 }
+
