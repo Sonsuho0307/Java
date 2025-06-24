823095af95903cb9e2c10c55c12785d3333c5a2a
Sonsuho0307 <sksh237@naver.com>
2025-06-24 09:44:32 +0900

    6/24

---
+++ src/test/_Quiz_js.java
@@ -0,0 +1,12 @@
+package test;
+
+public class _Quiz_js {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println('*');
+            }
+
+        }
+    }
+}
