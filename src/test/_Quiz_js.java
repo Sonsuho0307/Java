340919ee709fbb8e027fa48940489958020eea1b
Sonsuho0307 <sksh237@naver.com>
2025-06-24 10:32:03 +0900

    6/24

--- src/test/_Quiz_js.java
+++ src/test/_Quiz_js.java
@@ -22,6 +22,29 @@ public class _Quiz_js {
 
             System.out.println("");
         }
+        System.out.println("---------");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < (5 - i); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println("");
+        }
+        System.out.println("-------");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (5 - i); j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+
+
+
+
     }
 }
 
